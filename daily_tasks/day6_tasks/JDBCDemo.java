89ed43007e927a8d8d2f17f6c7d82df4ef2e5060
Karthik Sharma <151902673+karthiksharma22@users.noreply.github.com>
2025-02-05 20:43:54 +0530

    Update JDBCDemo.java

--- daily_tasks/day6_tasks/JDBCDemo.java
+++ daily_tasks/day6_tasks/JDBCDemo.java
@@ -6,16 +6,22 @@ public class JDBCDemo {
 	public static void main(String[] args) throws SQLException,Exception {
 		// TODO Auto-generated method stub
 		Class.forName("com.mysql.cj.jdbc.Driver");
-		//Establish the connection
+
+		//Establishing connection with Connection class
 		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/techm?autoReconnect=true&useSSL=false","root","tiger");
-		//enable the STatement object to pass sql queries.
+		
+		//creating a statement
 		Statement stmt = con.createStatement();
-		//use the ResultSet
+		
+		//execute the statement (select statement) and store the result in ResultSet
 		ResultSet rs = stmt.executeQuery("select * from Student");
 		
 		//iterate the rows
+		//the database consists of Student Table which contains id(int) and name(varchar)
 		while(rs.next()) {
+			//col 1 gets the student id
 			System.out.println(rs.getInt(1));
+			// col 2 gets the student name
 			System.out.println(rs.getString(2));		
 		}
 		rs.close();
