44ec1cb6e4de5dcc36e13b3c4b9240e819b069cf
Karthik Sharma <151902673+karthiksharma22@users.noreply.github.com>
2025-02-13 10:22:35 +0530

    Update Task7.js

--- daily_tasks/12-02-2025_tasks/Task7.js
+++ daily_tasks/12-02-2025_tasks/Task7.js
@@ -1,8 +1,9 @@
+// checking which 1st january between 2014 and 2025 lies on a sunday
 for (let year = 2014; year <= 2050; year++) {
   const date = new Date(year, 0, 1);
   const dayOfWeek = date.getDay();
 
   if (dayOfWeek === 0) {
-    console.log(`January 1st, ${year} is a Sunday.`);
+    console.log(`1st January of the year ${year} is a Sunday.`);
   }
 }
