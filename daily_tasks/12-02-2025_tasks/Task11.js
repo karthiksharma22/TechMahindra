921e704cea158eefb91313d0d75701913e3e3ca0
Karthik Sharma <151902673+karthiksharma22@users.noreply.github.com>
2025-02-13 10:30:50 +0530

    Update Task11.js

--- daily_tasks/12-02-2025_tasks/Task11.js
+++ daily_tasks/12-02-2025_tasks/Task11.js
@@ -1,3 +1,5 @@
+// converting deg Celcius to deg Farenheit and vice versa
+
 function changeToDegFaren(celsius) {
   return (celsius * 9) / 5 + 32;
 }
@@ -6,10 +8,10 @@ function changeToDegCel(fahrenheit) {
   return ((fahrenheit - 32) * 5) / 9;
 }
 
-let celsius = 60;
+let celsius = 65;
 console.log(`${celsius}°C after changing is ${changeToDegFaren(celsius)}°F`);
 
-let fahrenheitInput = 45;
+let fahrenheitInput = 35;
 console.log(
   `${fahrenheitInput}°F after changing is ${changeToDegCel(fahrenheitInput)}°C`
 );
