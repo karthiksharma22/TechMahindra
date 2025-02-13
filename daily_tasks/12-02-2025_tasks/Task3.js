a4c214fd9b791421e88037bdf00615da12b89829
Karthik Sharma <151902673+karthiksharma22@users.noreply.github.com>
2025-02-13 10:18:56 +0530

    Update Task3.js

--- daily_tasks/12-02-2025_tasks/Task3.js
+++ daily_tasks/12-02-2025_tasks/Task3.js
@@ -1,16 +1,8 @@
 let today = new Date();
-let date = today.getDate();
-let month = today.getMonth() + 1;
+let date = today.getDate().toString().padStart(2, "0");
+let month = (today.getMonth() + 1).toString().padStart(2, "0");
 let year = today.getFullYear();
-
-if (date < 10) {
-  date = "0" + date;
-}
-if (month < 10) {
-  month = "0" + month;
-}
 
 console.log(
   `month-date-year: ${month}-${date}-${year}, month/date/year: ${month}/${date}/${year}`
 );
-
