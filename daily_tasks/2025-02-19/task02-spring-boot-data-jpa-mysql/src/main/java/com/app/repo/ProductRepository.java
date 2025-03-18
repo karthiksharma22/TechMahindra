0ef3317aa6d7536a7960490b7de66ad3f29be511
Karthik Sharma <151902673+karthiksharma22@users.noreply.github.com>
2025-03-18 13:00:57 +0530

    Delete daily_tasks/2025-02-19 directory

--- daily_tasks/2025-02-19/task02-spring-boot-data-jpa-mysql/src/main/java/com/app/repo/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.app.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.app.model.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Integer> {
-}
